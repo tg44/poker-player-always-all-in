0edf92826ae8ae9f1a1f664393dbf8416b07bb6b
Gergo Torcsvari <gtorcsvari@emarsys.com>
2017-10-07 12:29:01 +0200

    10 midrange hand

--- src/main/scala/org/leanpoker/player/Player.scala
+++ src/main/scala/org/leanpoker/player/Player.scala
@@ -19,7 +19,7 @@ object Player extends JsonSupport {
   def generateResponse(state: GameState): Int = {
     val me = GameStateHelper.me(state)
     if(CardListHelper.naiveAllIn(me.hole_cards.get)) me.stack
-    else if (CardListHelper.midrangeHand(me.hole_cards.get)) me.stack
+    else if (CardListHelper.midrangeHand(me.hole_cards.get)) 20
     else 0
   }
 
