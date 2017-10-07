d1a22eb01b562330a9978d46164e64ba3d961c9f
Gergo Torcsvari <gtorcsvari@emarsys.com>
2017-10-07 10:30:38 +0200

    can parse json

--- src/main/scala/org/leanpoker/player/Player.scala
+++ src/main/scala/org/leanpoker/player/Player.scala
@@ -1,7 +1,7 @@
 package org.leanpoker.player
 
 import com.google.gson.JsonElement
-import spray.json.JsValue
+import spray.json.{DefaultJsonProtocol, JsValue, RootJsonFormat}
 
 object Player {
 
@@ -15,46 +15,16 @@ object Player {
 }
 
 
-class Dto {
-
-  val sample =
-    """{"players":[
-    {
-      "name":"Player 1",
-      "stack":1000,
-      "status":"active",
-      "bet":0,
-      "hole_cards":[],
-      "version":"Version name 1",
-      "id":0
-    },
-    {
-      "name":"Player 2",
-      "stack":1000,
-      "status":"active",
-      "bet":0,
-      "hole_cards":[],
-      "version":"Version name 2",
-      "id":1
-    }
-    ],
-    "tournament_id":"550d1d68cd7bd10003000003",
-    "game_id":"550da1cb2d909006e90004b1",
-    "round":0,
-    "bet_index":0,
-    "small_blind":10,
-    "orbits":0,
-    "dealer":0,
-    "community_cards":[],
-    "current_buy_in":0,
-    "pot":0
-  }"""
-
-}
-
-case class Player(name: String, stack: Int, status: String, bet: Int, hole_cards: JsValue, version: String, id: Int)
+case class PlayerDto(name: String,
+                  stack: Int,
+                  status: String,
+                  bet: Int,
+                  hole_cards: Seq[Card],
+                  version: String,
+                  id: Int)
 
 case class GameState(
+                    players: Seq[PlayerDto],
                       tournament_id: String,
                      game_id: String,
                       round: Int,
@@ -62,7 +32,16 @@ case class GameState(
                       small_blind: Int,
                       orbits: Int,
                       dealer: Int,
-                      community_cards: JsValue,
+                      community_cards: Seq[Card],
                       current_buy_in: Int,
                       pot: Int
                     )
+
+case class Card(rank: String, suit: String)
+
+trait JsonSupport extends DefaultJsonProtocol {
+
+  implicit val cardJsonFormatter: RootJsonFormat[Card] = jsonFormat2(Card)
+  implicit val playerJsonFormatter: RootJsonFormat[PlayerDto] = jsonFormat7(PlayerDto)
+  implicit val gameStateJsonFormatter: RootJsonFormat[GameState] = jsonFormat11(GameState)
+}
